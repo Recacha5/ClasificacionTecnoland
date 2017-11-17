381706f9f9f579fc63d477537ba80cde214b88a9
alumno <alumno@aula01-14>
2017-11-17 18:32:55 +0100

    Añadido algun metodo

---
+++ ClasificacionTecnoland/src/clasificaciontecnoland/Jugador.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clasificaciontecnoland;
+
+/**
+ *
+ * @author Alex Recacha
+ * 
+ */
+public class Jugador {
+    
+    private String nombre;
+    private int edad;
+    private int puntuacion;
+
+    public Jugador(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public Jugador(int puntuacion) {
+        this.puntuacion = puntuacion;
+    }
+    
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the edad
+     */
+    public int getEdad() {
+        return edad;
+    }
+
+    /**
+     * @param edad the edad to set
+     */
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    /**
+     * @return the puntuacion
+     */
+    public int getPuntuacion() {
+        return puntuacion;
+    }
+
+    /**
+     * @param puntuacion the puntuacion to set
+     */
+    public void setPuntuacion(int puntuacion) {
+        this.puntuacion = puntuacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre + "\n" +
+               "Edad: " + edad + "\n" +
+               "Puntuación: " + puntuacion;
+    }
+}
